ca6dbdd0e4aabbcccd55b8722bbf7b3b44840092
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-01-22 22:33:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+//your JS code here. If required.
+document.getElementById("btn").addEventListener("click",async ()=>{
+	let text = document.getElementById("text").value;
+	let delay = document.getElementById("delay").value;
+
+	if(text&&delay){
+		let myPromise = new Promise((resolve)=>{
+			setTimeout(()=>{
+				resolve(text);
+			},delay)
+		});
+
+		// let text2 = await myPromise;
+		// if(text2){
+		// 	document.getElementById("output").textContent = text2;
+		// }
+
+		// myPromise.then((data)=>{
+		// 	document.getElementById("output").textContent = data;
+		// })
+		
+	}
+
+	
+});
